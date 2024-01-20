c4a21581da0d10d097490d57cb92bfac738af8e1
YASHWANTH J <jy2723@srmist.edu.in>
2024-01-20 10:54:06 +0530

    [yashwanth] added create a tuple with diiffrent datatypes]

---
+++ tuple_with_diferent_datatypes.py
@@ -0,0 +1,7 @@
+user_input = input("Enter elements for the tuple separated by spaces: ")
+
+elements = user_input.split()
+
+mixed_tuple = tuple(elements)
+
+print("Different Data Type Tuple:", mixed_tuple)
