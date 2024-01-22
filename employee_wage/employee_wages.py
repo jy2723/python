31e253d72e3b93e2a8c9d298ba6793bfbd010c8f
YASHWANTH J <jy2723@srmist.edu.in>
2024-01-22 11:12:14 +0530

    [yashwanth] added calculate by total number of working hoursand days

--- employee_wage/employee_wages.py
+++ employee_wage/employee_wages.py
@@ -1,25 +1,38 @@
 import random
 
-def present_case():
-    print("Employee is Present\nwage:", daily_wage * 8)
-    print("monthly wage :",daily_wage*8*20)
+def calculate_daily_wage(hours_worked, daily_wage):
+    return daily_wage * hours_worked
 
-def part_time_case():
-    print("parttime wage:", daily_wage * 4)
-    print("monthly wage :",daily_wage*4*20)
+def calculate_monthly_wage(daily_wage, hours_per_day, days_per_month):
+    return daily_wage * hours_per_day * days_per_month
 
+def calculate_wages(target_hours, target_days, daily_wage, hours_per_day):
+    total_hours_worked = 0
+    total_days_worked = 0
 
-def absent_case():
-    print("Employee is Absent")
+    while total_hours_worked < target_hours and total_days_worked < target_days:
+        attendance_status = random.randint(0, 2)
 
-attendance_status = random.randint(0, 2)
+        if attendance_status == 1:  
+            hours_worked_today = 8  
+            total_hours_worked += hours_worked_today
+            total_days_worked += 1
+
+        elif attendance_status == 2:  
+            hours_worked_today = 4  
+            total_hours_worked += hours_worked_today
+            total_days_worked += 1
+
+        else:  
+            pass
+    total_wages = calculate_monthly_wage(daily_wage, hours_per_day, total_days_worked)
+    print(f"\nTotal Hours Worked: {total_hours_worked}")
+    print(f"Total Days Worked: {total_days_worked}")
+    print(f"Total Wages: {total_wages}")
+
+target_hours = 100
+target_days = 20
 daily_wage = 20
+hours_per_day = 8
 
-
-switch = {
-    1: present_case,
-    2: part_time_case,
-    0: absent_case
-}
-
-switch.get(attendance_status, absent_case)()
+calculate_wages(target_hours, target_days, daily_wage, hours_per_day)
