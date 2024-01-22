edf05c1f6321975656a7187eff99a4a1a7b01886
YASHWANTH J <jy2723@srmist.edu.in>
2024-01-22 10:15:25 +0530

    [yashwanth] added employee wage problem using switch case

---
+++ employee_wage/uc4.py
@@ -0,0 +1,21 @@
+import random
+
+def present_case():
+    print("Employee is Present\nwage:", daily_wage * 8)
+
+def part_time_case():
+    print("parttime wage:", daily_wage * 4)
+
+def absent_case():
+    print("Employee is Absent")
+
+attendance_status = random.randint(0, 2)
+daily_wage = 20
+
+switch = {
+    1: present_case,
+    2: part_time_case,
+    0: absent_case
+}
+
+switch.get(attendance_status, absent_case)()
